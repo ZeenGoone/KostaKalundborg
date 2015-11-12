99c1cbc34de2af26885d97dafbee59422001aea6
zeengoone <s974489@student.dtu.dk>
2015-11-12 13:16:32 +0100

    fixed lidt på campchief

--- src/entity/persons/CampChief.java
+++ src/entity/persons/CampChief.java
@@ -1,9 +1,13 @@
 package entity.persons;
 
 public class CampChief extends Employee {
-
+	boolean administrator = true;
+	
 	CampChief(String name, String phonenumber, String password) {
 		super(name, phonenumber, password);
+	}
+	public boolean getAdministrator(){
+		return administrator;
 	}
 
 }
