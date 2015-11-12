6d67947cff9e86692b378f4a3a81ce91047bfaca
Blackflunk <Alexander12350@hotmail.com>
2015-11-12 14:59:21 +0100

    Login logic opdateret

--- src/logic/LoginLogic.java
+++ src/logic/LoginLogic.java
@@ -1,5 +1,34 @@
 package logic;
+import java.util.ArrayList;
+
+import entity.persons.CampChief;
+import entity.persons.Employee;
+import entity.persons.Receptionist;
+import logic.DatabaseLogic;
 
 public class LoginLogic {
-
+	DatabaseLogic databaselogic;
+	private ArrayList<Receptionist> receptionistlist = new ArrayList<Receptionist>();
+	private ArrayList<CampChief> campchieflist = new ArrayList<CampChief>();
+	
+	public LoginLogic(ArrayList<CampChief> campchieflist, ArrayList<Receptionist> receptionistlist){
+		this.campchieflist = campchieflist;
+		this.receptionistlist = receptionistlist;
+	}
+	
+	
+	public Employee verifyUser( String phonenumber, String password){
+		
+		for(Receptionist r : receptionistlist){
+			if(phonenumber == r.getPhonenumber() && password == r.getPassword())
+				return r;
+		}
+		for(CampChief c : campchieflist){
+			if(phonenumber == c.getPhonenumber() && password == c.getPassword()){
+				return c;
+		}
+	}
+	return null;
+}
+		
 }
