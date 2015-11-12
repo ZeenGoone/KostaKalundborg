21fe4083913a8db2acd68dc0bc8a0c7fa7c6bb2e
AoschkA <jonaspraem@gmail.com>
2015-11-12 11:54:31 +0100

    logic

---
+++ src/logic/InvoiceLogic.java
@@ -0,0 +1,16 @@
+package logic;
+
+import entity.persons.Customer;
+
+public class InvoiceLogic {
+	
+	public InvoiceLogic() {}
+	
+	public String getInvoice(Customer customer) {
+		return "Customer: "+customer.getName()+"\t Phonenumber: "+customer.getPhonenumber()+
+				"\n"+
+				for (int i=0, customer.invoices.size()) {
+				
+	}
+
+}
