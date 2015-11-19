2c99739f10f3c64770abb7da0a545455cdec9f0c
Blackflunk <Alexander12350@hotmail.com>
2015-11-19 14:58:38 +0100

    rettelser

--- src/entity/reservation/Rental.java
+++ src/entity/reservation/Rental.java
@@ -2,115 +2,17 @@ package entity.reservation;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import entity.persons.Customer;
+import entity.tenancy.Tenancy;
 
 public class Rental extends Reservation {
 
-	
-	
-	// Customer customer = new Customer(name, phonenumber);
-	//Tenancy
-	String startDate; // in format yyyymmdd
-	String endDate;   // in format yyyymmdd
-	
-	
-	Rental (){
-		
+	public Rental(Customer customer, Tenancy[] tenancy, Date start_date,
+			Date end_date) {
+		super(customer, tenancy, start_date, end_date);
 	}
-	
-	public String getStartDate () {
-		return startDate;
-	}
-	
-	public String getEndDate () {
-		return endDate;
-	}
-	
-	public void setStartDate(String startDate){
-		this.startDate=startDate;
-	}
-	
-	public void setEndDate(String endDate){
-		this.endDate=endDate;
-	
-	}
-	
-/*	public void checkStartDate (){
-		String y = startDate.substring(0, 4);
-		int year = 0;
-		String m = startDate.substring(5,6);
-		int month = 0;
-		String d = startDate.substring(7,8);
-		int day= 0;
-			
-		try {
-				year = Integer.parseInt(y);
-				
-			} catch (NumberFormatException e){
-				System.err.println("Wrong start year format");
-				
-			}
-	
-		try {
-		
-			month = Integer.parseInt(m);
-			
-		} catch (NumberFormatException e){
-			System.err.println("Wrong start month format");
-			
-		}
-		
-		try {
-			day = Integer.parseInt(d);
-			
-		} catch (NumberFormatException e){
-			System.err.println("Wrong start day format");
-			
-		}
-		
-		if (year>1900 && year<2100){
-			System.out.println("Wrong year");
-				
-		}
-		else if (month<0 && month>12){
-			System.out.println("Wrong month");
-			
-		}
-		
-		else if(day<0 && day>31){ 
-			System.out.println("Wrong day");
-		}
-	
-		else if((month == 4 ||month == 6 ||month == 9 || month ==11 )&& (day>30)) { 
-			System.out.println("Wrong day");
-		}
-		
-		else if ((month==2 && year%4 !=0 && day>28) || (month==2 && year%4==0 && day>29)){
-			System.out.println("Wrong day");
-		}
-	}
-	*/
-	public boolean isValidStartDate (String startDate){
-		
-		SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
-		try{
-			df.parse(startDate);
-			return true;
-		} catch (ParseException e) {
-			return false;
-		}
-		
-	}
-	
-	public boolean isValidEndDate (String endDate){
-		SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
-		try{
-			df.parse(endDate);
-			return true;
-		} catch (ParseException e) {
-			return false;
-		}
-	}
+
 	
 }
--- src/entity/reservation/Reservation.java
+++ src/entity/reservation/Reservation.java
@@ -1,7 +1,59 @@
 package entity.reservation;
 
-public abstract class Reservation {
+import java.util.Date;
 
+import entity.persons.Customer;
+import entity.tenancy.Tenancy;
+
+public abstract class Reservation {
+	public Customer customer;
+	public Tenancy[] tenancy;
+	public Date start_date;
+	public Date end_date;
+	
+
+	public Reservation(Customer customer, Tenancy[] tenancy, Date start_date, Date end_date){
+		this.customer = customer;
+		this.tenancy = tenancy;
+		this.start_date = start_date;
+		this.end_date = end_date;
+
+	}
+
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Tenancy[] getTenancy() {
+		return tenancy;
+	}
+
+	public void setTenancy(Tenancy[] tenancy) {
+		this.tenancy = tenancy;
+	}
+
+	public Date getStart_date() {
+		return start_date;
+	}
+
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+	 
+	public Date getEnd_date() {
+		return end_date;
+	}
+	
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+	
+	
 	
 	
 }
